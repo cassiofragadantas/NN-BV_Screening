78b49a21b91ff899f26af24b88dae6863c7f04fc
Cassio F. Dantas <cassiofragadantas@gmail.com>
2022-06-27 17:02:03 +0200

    Modularizing Exp_BV.m by creating a separate function bvPGD.m for the PGD solver

--- exp/Exp_BV.m
+++ exp/Exp_BV.m
@@ -11,8 +11,8 @@ exp_type = 'synthetic'; % Options: 'synthetic', or some real dataset
                          %     'Cuprite_USGS-lib', 'Urban_USGS-lib' (using USGS spectral library as dictionary)
 
 % Dimensions (only for synthetic experiments, irrelevant otherwise)
-m = 2000;
-n = 1000;
+m = 1000;
+n = 500;
 screen_period = 10;
 normalizeA = true;
 
@@ -30,7 +30,10 @@ end
 
 %% Initializations
 nb_iter = 1e7; %maximum number of iterations
+mu = 2/norm(A).^2; % fixed stepsize
+x0 = abs(randn(n,1)); %random initialization for solution vector x
 
+% Data generation (A, y)
 if strcmp(exp_type,'synthetic')
     A = abs(randn(m,n)); A = A./sqrt(sum(A.^2)); % random A with unit-norm columns
     % random y
@@ -68,9 +71,6 @@ else
     u = 1*ones(n,1);
 end
 
-
-mu = 2/norm(A).^2; % fixed stepsize
-
 if strcmp(noise_type,'poisson')
     y = poissrnd(full(y_orig));
 elseif strcmp(noise_type,'gaussian_std') % gaussian with std
@@ -81,185 +81,32 @@ else % no noise
     y = y_orig;
 end
 
-beta = 2;
-% primal and dual cost functions
-if beta == 1
-    %KL divergence
-    epsAx = 0;
-    primal = @(a) sum(v .* log(y ./ (a+epsAx)),'omitnan') + sum(- y + a + epsAx); % 0*log0 = 0
-    dual = @(b, Atb) sum(y.*log(1+b),'omitnan') - sum(epsAx*b) - l.'*max(0,Atb) - u.'*min(0,Atb); % force 0*log(0) = 0 instead of NaN
-elseif beta == 2
-    %Euclidean distance (beta = 2)
-    primal = @(a) 0.5*sum( (y - a).^2 );
-    dual = @(b,Atb) 0.5*sum( y.^2 - (y - b).^2 ) - l.'*min(0,Atb) - u.'*max(0,Atb);    
-    primalScreen = @(yvec, a) 0.5*sum( (yvec - a).^2 );
-    dualScreen = @(yvec,b,Atb,lvec,uvec) 0.5*sum( yvec.^2 - (yvec - b).^2 ) - lvec.'*min(0,Atb) - uvec.'*max(0,Atb);
-elseif beta == 1.5
-    %Beta divergence beta = 1.5
-    epsAx = 0;    
-    primal = @(a) 4/3 * sum( (y).^1.5 + (1/2)*(a + epsAx).^(1.5) ...
-              - (3/2)*(y).*sqrt(a + epsAx)); %beta divergence      
-    dual = @(b, Atb) sum( (4*y.^1.5 + 0.5*b.^3 - 0.5*(b.^2 + 4*y).^1.5 + 3*b.*y)/3 - epsAx*b ) ...
-                - l.'*max(0,Atb) - u.'*min(0,Atb);
-else
-    error('Value of beta not implemented!')
-end
-
-%Screening initialization
-if beta == 2
-    normA = sqrt(sum(A.^2)).';
-else
-    error('Screening initialization not implemented for this value of beta')
-end
-
-% Storage variables
-cost = zeros(1,nb_iter); %save cost function evolution
-dualgap = zeros(1,nb_iter); %save cost function evolution
-timeIt = zeros(1,nb_iter); %save total time at each iteration
-costScreen = zeros(1,nb_iter); %save cost function evolution
-dualgapScreen = zeros(1,nb_iter); %save cost function evolution
-timeItScreen = zeros(1,nb_iter); %save total time at each iteration
-screen_ratio = zeros(1,nb_iter); %save screening ratio evolution
-saturated_coords = zeros(1,n); % saturated coords
-
 %% Find x such that y=Ax, given A and y
 % profile on
-x0 = abs(randn(n,1)); %random initialization
 
+%%%%%%%%%%%% Projected gradient (PGD) algorithm %%%%%%%%%%%%
+% profile on
 
-% No screening
-x = x0;
-Ax = A*x;
-startTime = tic;
-k=1; 
-gap = inf; calc_gap = true;
-delta_tol = 1e-8; delta_x = 1; delta_x0 = 0;
-while gap >= gap_tol && k <= nb_iter
-% while delta_x >= delta_tol*delta_x0 && k <= nb_iter
-    xprev = x;
-    % -- Primal update --
-    if beta == 2
-        % Projected gradient
-        res = y - Ax;
-        grad = -A.'*res;
-        x = x - mu*grad;
-        x(x<l) = l(x<l); x(x>u) = u(x>u); % projection step (saturate entries)
-        Ax = A*x;     
-    else
-        res = (y - Ax).*Ax.^(beta-2);
-        grad = -A.'*res;
-        error('Primal update not implemented for this beta')
-    end
-    
-    % -- Stopping criterion --
-%     delta_x = norm(x-xprev)^2;
-%     if k == 1, delta_x0 = delta_x; end
-
-    if calc_gap
-        % -- Dual update --
-        theta =  res; % simply the (generalized) residual
-        ATtheta = -grad;
-
-        % -- Duality gap -- 
-        cost(k) =  primal(Ax);
-        gap = cost(k) - dual(theta,ATtheta); % gap has to be calculated anyway for GAP_Safe
-        gap(gap<=0) = eps;
-        dualgap(k) = gap;    
-    end
-    timeIt(k) = toc(startTime);
-    k = k+1;
-end
-xBase = x;
-cost = cost(1:k-1);
-timeIt = timeIt(1:k-1);
-dualgap = dualgap(1:k-1);
-
-
-% Screening
-A0=A;
-u0 = u; l0=l;
-x = x0;
-Ax = A*x;
-startTime = tic;
-k=1; gap = inf;
-while gap >= gap_tol && k <= nb_iter 
-    % -- Primal update --
-    if beta == 2
-        % Projected gradient
-        res = y - Ax;
-        grad = -A.'*res;
-        x = x - mu*grad;
-        x(x<l) = l(x<l); x(x>u) = u(x>u); % projection step (saturate entries)
-        Ax = A*x;
-    else
-        res = (y - Ax).*Ax.^(beta-2);
-        grad = -A.'*res;
-        error('Primal update not implemented for this beta')
-    end
-
-    % -- Dual update --
-    theta =  res; % simply the (generalized) residual
-    ATtheta = -grad;
-
-    % -- Stopping criterion -- 
-    costScreen(k) =  primalScreen(y,Ax);
-    gap = costScreen(k) - dualScreen(y,theta,ATtheta,l,u); % gap has to be calculated anyway for GAP_Safe
-    gap(gap<=0) = eps;
-    dualgapScreen(k) = gap;
-    
-    % Screening
-    if mod(k,screen_period) == 0
-        if beta == 2
-            radius = sqrt(2*gap);
-            screen_vec_l = (ATtheta + radius*normA < 0);
-            screen_vec_u = (ATtheta - radius*normA > 0);
-        else
-            error('Screening not implemented for this beta.')
-        end
-        y = y - A(:,screen_vec_l)*l(screen_vec_l) - A(:,screen_vec_u)*u(screen_vec_u);
-        Ax = Ax - A(:,screen_vec_l)*x(screen_vec_l) - A(:,screen_vec_u)*x(screen_vec_u);    
-        A(:,screen_vec_l | screen_vec_u) = [];    
-        x(screen_vec_l | screen_vec_u) = []; 
-        normA(screen_vec_l | screen_vec_u) = [];
-        l(screen_vec_l | screen_vec_u) = [];
-        u(screen_vec_l | screen_vec_u) = [];
-        saturated_coords(~saturated_coords) = screen_vec_u - screen_vec_l;  
-    end
-    screen_ratio(k) = sum(abs(saturated_coords))/n;
-
-    timeItScreen(k) = toc(startTime);
-    k = k+1;
-end
-k = k-1;
-timeItScreen = timeItScreen(1:k);
-costScreen = costScreen(1:k);
-dualgapScreen = dualgapScreen(1:k);
-screen_ratio = screen_ratio(1:k);
-
-
-% zero-padding solution
-x_old = x;
-x = zeros(n,1);
-x(~saturated_coords) = x_old;
-x(saturated_coords == 1) = u0(saturated_coords == 1);
-x(saturated_coords == -1) = l0(saturated_coords == -1);
-A = A0;
+fprintf('\n======= Coord. Descent algorithm =======\n')
+[xPGD, outPGD]= bvPGD(y,A,l,u,x0,mu,nb_iter,calc_gap);
+[xPGD_screen, outPGD_screen]= bvPGD(y,A,l,u,x0,mu,nb_iter,calc_gap,screen_period);
 
 % Assert screening did not affect algorithm convergence point
-assert(norm(xBase - x)<=gap_tol*norm(x), 'Error! Screening changed the PG solver result')
+assert(norm(xPGD - xPGD_screen)<=gap_tol*norm(xPGD_screen), ...
+       'Error! Screening changed the PG solver result')
 
 % profile off, profsave(profile('info'),'./new_Profile_PG_BVLS')
 
 %% Results
 filename = ['new_Exp_BV_' exp_type '_m' num2str(m) 'n' num2str(n) ... 
             '_scrperiod' num2str(screen_period) '_noise-' noise_type ...
-            '_sp' num2str(nnz(x)/n) '_seed' num2str(rng_seed)];
+            '_sp' num2str(nnz(xPGD_screen)/n) '_seed' num2str(rng_seed)];
         
 if ~exist('omitResults','var')
     %%%% Show times %%%%
-    fprintf('PG algorithm : %.2f s\n', timeIt(end))
-    fprintf('PG + Screening : %.2f s\n', timeItScreen(end))
-    fprintf('PG speedup : %.2f times \n\n', timeIt(end)/timeItScreen(end))
+    fprintf('PG algorithm : %.2f s\n', outPGD.timeIt(end))
+    fprintf('PG + Screening : %.2f s\n', outPGD_screen.timeIt(end))
+    fprintf('PG speedup : %.2f times \n\n', outPGD.timeIt(end)/outPGD_screen.timeIt(end))
     
     if calc_gap
     % Plot properties setting
@@ -272,10 +119,10 @@ if ~exist('omitResults','var')
     %%%% Duality gap vs. Time %%%       
     subplot(2,1,1)
     % Baseline
-    semilogy(timeIt(idx),dualgap(idx),'k'),  hold on,
+    semilogy(outPGD.timeIt(idx),outPGD.gapIt(idx),'k'),  hold on,
     set(gca,'ColorOrderIndex',1)
     % Screening
-    semilogy(timeItScreen(idx),dualgapScreen(idx)), 
+    semilogy(outPGD_screen.timeIt(idx),outPGD_screen.gapIt(idx)), 
     % Settings
     ylabel('Duality gap'), xlabel('Time [s]'), grid on
     legend({'Prox. Grad. (baseline)', 'Prox. Grad + Screening', })
@@ -283,11 +130,11 @@ if ~exist('omitResults','var')
     %%%% Screening ratio vs. Time %%%        
     subplot(2,1,2), hold on
     % Oracle
-    nb_saturated = sum(xBase>=u0-eps) + sum(xBase<=l0+eps);
-    plot([0 timeIt(end)], repmat(nb_saturated/n,1,2),'--','color',0.5*[1 1 1])
+    nb_saturated = sum(xPGD>=u0-eps) + sum(xPGD<=l0+eps);
+    plot([0 outPGD.timeIt(end)], repmat(nb_saturated/n,1,2),'--','color',0.5*[1 1 1])
     % Screening
     set(gca,'ColorOrderIndex',1)
-    plot(timeItScreen(idx),screen_ratio(idx)), xlim([0 timeIt(end)]), ylim([0 1])
+    plot(outPGD_screen.timeIt(idx),outPGD_screen.screenIt(idx)), xlim([0 outPGD.timeIt(end)]), ylim([0 1])
     % Settings    
     ylabel('Screening ratio [\%]'), xlabel('Time [s]'), grid on
     legend({'Oracle' 'Screened' }, 'Location', 'southeast')
---
+++ misc/bvPGD.m
@@ -0,0 +1,125 @@
+function [x, output] = bvPGD(y,A,l,u,x,mu,maxiter,calc_gap,screen_period)
+% Projected gradient algorithm for the Bounded-Variable Linear Regression
+% problem. 
+%
+% min_{x | l_i <= x_i <= u_i} D(y, Ax)
+%
+% where the cost function D(y,Ax) is a beta-divergence between vectors y
+% and Ax (for now, only the case beta=2, i.e. the LS-case is implemented)
+%
+% Required inputs:
+% - y: (m x 1) input vector (to be approximated by the linear model A*x)
+% - A: (m x n) matrix
+% - l, u: (n x 1) vectors with the lower and upper bounds (respectively)
+%         for the solution vector x.
+% Optional inputs (default values):
+% - x: (n x 1) initialization vector (all-zeros vector )
+% - mu: step-size, if constant (2/norm(A)^2)
+% - maxiter: maximum allowed number of iterations (1e6)
+% - calc_gap: flag defining if the duality gap is to be computed (false)
+% - screen_period: interval between screening tests (0, i.e. no screening)
+%
+% Author: Cassio F. Dantas
+% Date: June 24th 2022
+
+% Default input variables
+if nargin < 4, error('bvPGD: Inputs {y, A, l, u} have to be provided'); end
+if nargin < 5, x = zeros(size(A,2),1); end
+if nargin < 6, mu=2/norm(A).^2; end
+if nargin < 7, maxiter = 1e6; end
+if nargin < 8, calc_gap = false; end
+if nargin < 9, screen_period = 0; end
+
+if screen_period % Safe screening activated
+    calc_gap = true; % Gap has to be computed for screening
+    normA = sqrt(sum(A.^2)).';
+    saturated_coords = zeros(size(x)); % tracks the saturated coordinates
+    n = length(x); u0 = u; l0=l;
+end
+
+% Output variables
+output.timeIt = zeros(1,maxiter);
+output.costIt = zeros(1,maxiter);
+if calc_gap, output.gapIt = zeros(1,maxiter); end
+if screen_period, output.screenIt = zeros(1,maxiter); end
+
+% Initializations
+Ax = A*x;
+startTime = tic;
+k=1; 
+gap = inf;
+gap_tol = 1e-6;
+
+% Cost functions (primal and dual)
+% Euclidean distance
+primal = @(yvec, a) 0.5*sum( (yvec - a).^2 );
+dual = @(yvec,b,Atb,lvec,uvec) 0.5*sum( yvec.^2 - (yvec - b).^2 ) - lvec.'*min(0,Atb) - uvec.'*max(0,Atb);
+% KL divergence (solver not implemented)
+% epsAx = 0;
+% primal = @(a) sum(v .* log(y ./ (a+epsAx)),'omitnan') + sum(- y + a + epsAx); % 0*log0 = 0
+% dual = @(b, Atb) sum(y.*log(1+b),'omitnan') - sum(epsAx*b) - l.'*max(0,Atb) - u.'*min(0,Atb); % force 0*log(0) = 0 instead of NaN
+
+% Main loop
+while gap >= gap_tol && k <= maxiter
+% delta_tol = 1e-8; delta_x = 1; delta_x0 = 0;    
+% while delta_x >= delta_tol*delta_x0 && k <= maxiter
+%     xprev = x;
+
+    % -- Primal update --
+    res = y - Ax;
+    grad = -A.'*res;
+    x = x - mu*grad;
+    x(x<l) = l(x<l); x(x>u) = u(x>u); % projection step (saturate entries)
+    Ax = A*x;
+    
+    if calc_gap
+        % -- Dual update --
+        theta =  res; % simply the (generalized) residual
+        ATtheta = -grad;
+
+        % -- Duality gap -- 
+        output.costIt(k) =  primal(y,Ax);
+        gap = output.costIt(k) - dual(y,theta,ATtheta,l,u);
+        gap(gap<=0) = eps;
+        output.gapIt(k) = gap;    
+    end
+    
+    % -- Stopping criterion --
+%     delta_x = norm(x-xprev)^2;
+%     if k == 1, delta_x0 = delta_x; end
+    
+
+    % Screening
+    if mod(k,screen_period) == 0
+        radius = sqrt(2*gap);
+        screen_vec_l = (ATtheta + radius*normA < 0);
+        screen_vec_u = (ATtheta - radius*normA > 0);
+
+        y = y - A(:,screen_vec_l)*l(screen_vec_l) - A(:,screen_vec_u)*u(screen_vec_u);
+        Ax = Ax - A(:,screen_vec_l)*x(screen_vec_l) - A(:,screen_vec_u)*x(screen_vec_u);    
+        A(:,screen_vec_l | screen_vec_u) = [];    
+        x(screen_vec_l | screen_vec_u) = []; 
+        normA(screen_vec_l | screen_vec_u) = [];
+        l(screen_vec_l | screen_vec_u) = [];
+        u(screen_vec_l | screen_vec_u) = [];
+        saturated_coords(~saturated_coords) = screen_vec_u - screen_vec_l;          
+    end
+    if screen_period, output.screenIt(k) = sum(abs(saturated_coords))/n; end
+
+    output.timeIt(k) = toc(startTime);
+    k = k+1;
+end
+
+output.costIt = output.costIt(1:k-1);
+output.timeIt = output.timeIt(1:k-1);
+output.gapIt = output.gapIt(1:k-1);
+if screen_period, output.screenIt = output.screenIt(1:k-1); end
+
+% zero-padding solution
+if screen_period
+    x_old = x;
+    x = zeros(n,1);
+    x(~saturated_coords) = x_old;
+    x(saturated_coords == 1) = u0(saturated_coords == 1);
+    x(saturated_coords == -1) = l0(saturated_coords == -1);
+end
