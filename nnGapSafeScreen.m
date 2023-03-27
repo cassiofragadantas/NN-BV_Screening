d3e13115ac4730ea539e5958f27fb6ede29951e0
Cassio F. Dantas <cassiofragadantas@gmail.com>
2023-03-27 17:11:54 +0200

    [cosmetic] Variable renaming

--- nnGapSafeScreen.m
+++ nnGapSafeScreen.m
@@ -1,4 +1,4 @@
-function [screen_vec, trace] = nnGapSafeScreen(y, A, res, ATres, normA, sumA,t,precalc)
+function [screen_vec, trace] = nnGapSafeScreen(y, A, res, ATres, normA, Atdual,t,options)
 % Returns a n-dimensional logic vector with true entries indicating the
 % saturated (zero) coordinates.
 %
@@ -10,31 +10,32 @@ function [screen_vec, trace] = nnGapSafeScreen(y, A, res, ATres, normA, sumA,t,p
 %   res: residual (y-Ax in the LS case)
 %   ATres: A.'*res
 %   normA: (n x 1) vector containing the L2 norms of the columns of A
-%   sumA: (n x 1) vector containing the columwise sum of the entries of A 
+%   Atdual: (n x 1) vector containing the dot product A.'*tdual
 
 % Handle inputs (should be provided by the user for competitive results)
+% default values correspond to the LS case only
 if nargin < 2, error('Two first input parameters (y and A) are mandatory.')
 elseif nargin < 3, res = y - A*x;
 elseif nargin < 4, ATres = A.'*res;
 elseif nargin < 5, normA = sqrt(sum(A,1).^2).';
-elseif nargin < 6, sumA = sum(A,1); 
+elseif nargin < 6, Atdual = sum(A,1); 
 elseif nargin < 7, t = 1; 
 end
 
-% Primal and dual cost functions (LS case)
+% Primal and dual cost functions
 primal = @(a) 0.5*sum( (y - a).^2 );
 dual = @(b) 0.5*sum( y.^2 - (y - b).^2 );
 
 % -- Dual update --
-if exist('precalc','var') && isfield(precalc,'oracle_theta')
+if exist('options','var') && isfield(options,'oracle_theta')
     % Oracle dual point
-    theta = precalc.oracle_theta;
-    ATtheta = precalc.oracle_ATtheta; 
+    theta = options.oracle_theta;
+    ATtheta = options.oracle_ATtheta; 
 else
     % Dual feasible point    
-    epsilon = max(ATres./sumA.'); %max(ATres) also works if A is normalized, but is slightly worse.
+    epsilon = max(ATres./Atdual.'); %max(ATres) also works if A is normalized, but is slightly worse.
     theta = res - epsilon*t;
-    ATtheta = ATres - epsilon*sumA.'; %= A.'* theta; Should be zero at coordinates xj ~= 0 and negative otherwise    
+    ATtheta = ATres - epsilon*Atdual.'; %= A.'* theta; Should be zero at coordinates xj ~= 0 and negative otherwise    
 end
 
 
